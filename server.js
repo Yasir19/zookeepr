6dd60bdae49ecaa67dc43ea49a6386e0995c8f11
yasir19 <yasir.habboo@gmail.com>
2022-01-17 18:30:47 -0600

    handel req for a specific animal

--- server.js
+++ server.js
@@ -32,6 +32,10 @@ const filterByQuery = (query, animalsArray) => {
     }
     return filteredResults;
 }
+const findById = (id, animalsArray) =>{
+    const result = animalsArray.filter(animal => animal.id === id)[0]
+    return result;
+}  
 app.get('/api/animals',(req, res) =>{
     let results = animals;
     if(req.query) {
@@ -39,6 +43,14 @@ app.get('/api/animals',(req, res) =>{
     }
     res.json(results);
 });
+app.get('/api/animals/:id',(req,res)=>{
+    const result = findById(req.params.id, animals);
+    if (result){
+    res.json(result);
+    } else {
+        res.send(404);
+    }
+    });
 app.listen(PORT,()=>{
     console.log(`API server now on port ${PORT}!`);
 });
